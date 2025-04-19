d73b8bfaad8252ddbdcce13531329e56b1169f5c
Sriram Nambakam <snambakam@gmail.com>
2025-04-18 18:43:57 -0700

    Add more tools to the development environment

--- fedora/setup-dev-env.sh
+++ fedora/setup-dev-env.sh
@@ -3,8 +3,17 @@
 sudo dnf install -y \
 	autoconf \
 	automake \
+	curl \
 	gcc \
+	g++ \
 	gdb \
 	git \
 	golang \
-	libtool
+	libtool \
+	wget
+
+#
+# Install Rust
+#
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
